fe0c2f658b96763c9594b6458b5d218a160bf816
Dacio Romero <dacioromero@gmail.com>
2022-12-09 02:48:25 -0800

    updates

--- overlays/mullvad-vpn.nix
+++ overlays/mullvad-vpn.nix
@@ -1,7 +1,7 @@
-# Mullvad needs forced Wayland hints, their script doesn't support Gnome as a compositor
+# Mullvad needs forced Wayland hints because their script doesn't support Gnome as a compositor
 # https://github.com/mullvad/mullvadvpn-app/issues/3062
 # https://github.com/mullvad/mullvadvpn-app/blob/9639b2ceab5ec9c7696204e27f5b87dcc09a7a82/dist-assets/linux/mullvad-gui-launcher.sh#L11
-# Still will crash if opened from tray on Gnome
+# Still will crash if opened from tray on Wayland, so we're forcing X11 mode
 # https://github.com/electron/electron/issues/35657
 final: prev: {
   mullvad-vpn = prev.symlinkJoin {
@@ -10,7 +10,8 @@ final: prev: {
     buildInputs = [prev.makeWrapper];
     postBuild = ''
       wrapProgram $out/bin/mullvad-vpn \
-        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}"
+        --add-flags "--disable-features=UseOzonePlatform --use-gl=desktop"
+      sed -i "s|Exec.*$|Exec=$out/bin/mullvad-vpn %U|" $out/share/applications/mullvad-vpn.desktop
     '';
   };
 }
