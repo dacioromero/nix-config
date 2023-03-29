23cdcf1f30b0001b45ada84568f162888cd55176
Dacio Romero <dacioromero@gmail.com>
2023-03-28 20:36:38 -0700

    package xwayland video bridge

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,8 @@
 { pkgs }:
+let
+  callPackage = pkgs.lib.callPackageWith pkgs;
+in
 with pkgs; {
   satisfactory-mod-manager = callPackage ./satisfactory-mod-manager.nix { };
+  xwaylandvideobridge = callPackage ./xwaylandvideobridge.nix { };
 }
