52d6063e25807cce7d610906dfbea9ded5da46c5
Dacio Romero <dacioromero@gmail.com>
2023-12-29 23:30:07 -0800

    update 2023-12-30

--- hosts/fiyarr/configuration.nix
+++ hosts/fiyarr/configuration.nix
@@ -1,6 +1,7 @@
 { pkgs
 , inputs
 , lib
+, config
 , ...
 }: {
   imports =
@@ -37,12 +38,53 @@
     intel-media-driver
     intel-compute-runtime
   ];
-  services.sonarr = {
-    enable = true;
-    openFirewall = true;
-    user = "media";
-    group = "media";
+  virtualisation.oci-containers.containers.sonarr = {
+    image = "lscr.io/linuxserver/sonarr:develop-4.0.0.738-ls17";
+    environment = {
+      PUID = toString config.users.users.media.uid;
+      PGID = toString config.users.groups.media.gid;
+      TZ = config.time.timeZone;
+    };
+    volumes = [
+      "/var/lib/sonarr-container:/config"
+      "/media:/media"
+    ];
+    ports = [ "8989:8989" ];
   };
+  virtualisation.oci-containers.containers.heimdall = {
+    image = "lscr.io/linuxserver/heimdall:V2.5.8-ls245";
+    environment = {
+      PUID = toString config.users.users.media.uid;
+      PGID = toString config.users.groups.media.gid;
+      TZ = config.time.timeZone;
+    };
+    volumes = [
+      "/var/lib/heimdall:/config"
+      "/media:/media"
+    ];
+    ports = [
+      "80:80"
+      "443:443"
+    ];
+  };
+  virtualisation.oci-containers.containers.flaresolverr = {
+    image = "ghcr.io/flaresolverr/flaresolverr:v3.3.12";
+    environment = {
+      TZ = config.time.timeZone;
+    };
+    ports = [ "8191:8191" ];
+  };
+  networking.firewall.allowedTCPPorts = [
+    8989
+    80
+    443
+  ];
+  # services.sonarr = {
+  #   enable = true;
+  #   openFirewall = true;
+  #   user = "media";
+  #   group = "media";
+  # };
   services.radarr = {
     enable = true;
     openFirewall = true;
