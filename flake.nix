f3a22599b83cc25e660e9c69f0380e15af43666f
Dacio Romero <dacioromero@gmail.com>
2024-05-18 01:04:30 -0700

    lock: update 2024-05-18

--- flake.nix
+++ flake.nix
@@ -34,7 +34,6 @@
     pre-commit-hooks = {
       url = "github:cachix/pre-commit-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
       inputs.flake-compat.follows = "flake-compat";
     };
     lanzaboote = {
