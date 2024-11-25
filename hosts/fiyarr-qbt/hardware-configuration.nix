653f8321cc4245a5c0847e535f8d36b22f93adff
Dacio Romero <dacioromero@gmail.com>
2024-11-24 19:39:36 -0800

    update hw-cfgs

--- hosts/fiyarr-qbt/hardware-configuration.nix
+++ hosts/fiyarr-qbt/hardware-configuration.nix
@@ -11,7 +11,7 @@
 
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
