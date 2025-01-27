27978ef4ce3dd129c8f65457aaae513fb2828122
Dacio Romero <dacioromero@gmail.com>
2025-01-27 05:04:29 -0800

    fiyarr-qbt: update vpn endpoint

--- hosts/fiyarr-qbt/configuration.nix
+++ hosts/fiyarr-qbt/configuration.nix
@@ -76,7 +76,7 @@
     wireguardPeers = [{
       PublicKey = "PyLCXAQT8KkM4T+dUsOQfn+Ub3pGxfGlxkIApuig+hk=";
       PresharedKeyFile = config.age.secrets.airvpn-preshared-key.path;
-      Endpoint = "198.44.134.21:1637";
+      Endpoint = "198.44.134.6:1637";
       AllowedIPs = [ "0.0.0.0/0" "::/0" ];
       # AirVPN specified
       PersistentKeepalive = 15;
